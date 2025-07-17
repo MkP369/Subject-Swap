8733f5c475cad0c8e696f0308ab12593aeda86a8
s-reeshma <sree.cs123456@gmail.com>
2025-07-17 13:56:02 +0300

    database models

---
+++ backend/models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email:    { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  age: Number,
+  board: String,
+  phone: String,
+  userClass: String,
+  language: String
+});
+
+// 👇 This is what you should export
+module.exports = mongoose.model('User', userSchema);
